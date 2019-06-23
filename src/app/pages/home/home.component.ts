6751b4dbbaf6009390b32d2357e2527ee7a1520f
Johnson Huynh <huynhjjk@gmail.com>
2019-06-23 10:50:25 -0700

    disabling resolver

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
+import { SeoService } from '../../services/seo/seo.service';
+import { environment } from '../../../environments/environment';
 import * as _ from 'lodash';
 @Component({
   selector: 'app-home',
@@ -7,12 +9,25 @@ import * as _ from 'lodash';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
+  public environment = environment;
   public data;
 
   constructor(
-    private activatedRoute: ActivatedRoute
+    private seoService: SeoService,
+    private http: HttpClient
   ) {
-    this.data = this.activatedRoute.snapshot.data.data;
+
+    this.http.get(`https://api.myjson.com/bins/12qook`).toPromise()
+    .then(response => {
+      if (!_.isNil(response)) {
+        this.data = response;
+      }
+    }).catch(error => {});
+
+    this.seoService.setMetaTags({
+      title: `Custom Homepage Title`,
+      description: `Manually putting custom description here`
+    });
   }
 
   ngOnInit() {}
