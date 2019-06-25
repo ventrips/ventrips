a873998fae0c5671025787ae544422d43f492e5f
Johnson Huynh <huynhjjk@gmail.com>
2019-06-24 22:33:47 -0700

    fixing firebase connection

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,14 +1,14 @@
 export const environment = {
   production: true,
-  name: 'ventrips',
-  description: 'ventrips description',
+  name: 'Ventrips',
+  description: 'A place where you can get all of the latest news and trends',
   url: 'https://ventrips.firebaseapp.com',
   firebase: {
     apiKey: 'AIzaSyDAb0y87YBB3GH6Qddod_PDiUqjFxpC1gQ',
-    authDomain: 'ventrips.firebaseapp.com',
-    databaseURL: 'https://ventrips.firebaseio.com',
-    projectId: 'ventrips',
-    storageBucket: 'ventrips.appspot.com',
+    authDomain: 'ventrips-website.firebaseapp.com',
+    databaseURL: 'https://ventrips-website.firebaseio.com',
+    projectId: 'ventrips-website',
+    storageBucket: 'ventrips-website.appspot.com',
     messagingSenderId: '556585707956',
     appId: '1:556585707956:web:752f1d8777908fad'
   }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,15 +4,15 @@
 
 export const environment = {
   production: false,
-  name: 'ventrips',
-  description: 'ventrips description',
-  url: 'https://ventrips.firebaseapp.com',
+  name: 'Ventrips',
+  description: 'A place where you can get all of the latest news and trends',
+  url: 'https://ventrips-website.firebaseapp.com',
   firebase: {
     apiKey: 'AIzaSyDAb0y87YBB3GH6Qddod_PDiUqjFxpC1gQ',
-    authDomain: 'ventrips.firebaseapp.com',
-    databaseURL: 'https://ventrips.firebaseio.com',
-    projectId: 'ventrips',
-    storageBucket: 'ventrips.appspot.com',
+    authDomain: 'ventrips-website.firebaseapp.com',
+    databaseURL: 'https://ventrips-website.firebaseio.com',
+    projectId: 'ventrips-website',
+    storageBucket: 'ventrips-website.appspot.com',
     messagingSenderId: '556585707956',
     appId: '1:556585707956:web:752f1d8777908fad'
   }
