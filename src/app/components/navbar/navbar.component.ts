29056373d4e1677ad97606fd1747555e25bf9265
Johnson Huynh <huynhjjk@gmail.com>
2019-06-28 18:51:04 -0700

    adding login nav

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -23,7 +23,7 @@ export class NavbarComponent implements OnInit {
 
   constructor(
     private router: Router,
-    private authService: AuthService
+    public authService: AuthService
   ) { }
 
   ngOnInit() {
