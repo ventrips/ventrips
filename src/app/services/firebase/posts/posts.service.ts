54910928c4791e0ac11e37f60d7cc60d97ac1798
Johnson Huynh <huynhjjk@gmail.com>
2019-06-30 22:51:40 -0700

    mock data

--- src/app/services/firebase/posts/posts.service.ts
+++ src/app/services/firebase/posts/posts.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Post } from './../../../interfaces/post';
+import { MockData } from './../../../../assets/mocks/mockData';
 import * as _ from 'lodash';
 import * as faker from 'faker';
 
@@ -7,21 +8,7 @@ import * as faker from 'faker';
   providedIn: 'root'
 })
 export class PostsService {
-  public posts: Array<Post> = _.map(_.range(0, 6), (item) => {
-    const post: Post = {
-      slug: faker.lorem.slug(),
-      uid: faker.random.uuid(),
-      topic: faker.name.jobArea(),
-      title: faker.name.title(),
-      description: faker.lorem.sentence(),
-      image: faker.image.image(),
-      body: faker.lorem.sentences(),
-      created: faker.date.past(),
-      modified: faker.date.recent(),
-      published: true
-    };
-    return post;
-  });
+  public posts = MockData.postsJSON;
 
   constructor() {}
 
