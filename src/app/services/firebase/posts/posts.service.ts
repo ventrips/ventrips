4fbcf8d140fa0c71c0c9a3b117cd7c68a149e6a8
Johnson Huynh <huynhjjk@gmail.com>
2019-07-03 09:40:18 -0700

    cleaning up

--- src/app/services/firebase/posts/posts.service.ts
+++ src/app/services/firebase/posts/posts.service.ts
@@ -1,6 +1,5 @@
 import { Injectable } from '@angular/core';
 import { Post } from './../../../interfaces/post';
-import { MockData } from './../../../../assets/mocks/mockData';
 import * as _ from 'lodash';
 import * as faker from 'faker';
 
@@ -8,15 +7,5 @@ import * as faker from 'faker';
   providedIn: 'root'
 })
 export class PostsService {
-  public posts = MockData.postsJSON;
-
   constructor() {}
-
-  getPostByUID(uid: string) {
-    return _.filter(this.posts, (post) => _.isEqual(post.uid, uid));
-  }
-
-  getPosts() {
-    return this.posts;
-  }
 }
