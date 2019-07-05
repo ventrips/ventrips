f62cbff43074c025a913d4ebcf933f523fd8b85e
Johnson Huynh <huynhjjk@gmail.com>
2019-07-04 23:22:39 -0700

    adding time ago

--- src/app/pipes/time-ago/time-ago.pipe.spec.ts
+++ src/app/pipes/time-ago/time-ago.pipe.spec.ts
@@ -2,7 +2,7 @@ import { TimeAgoPipe } from './time-ago.pipe';
 
 describe('TimeAgoPipe', () => {
   it('create an instance', () => {
-    const pipe = new TimeAgoPipe();
+    const pipe = new TimeAgoPipe(null, null);
     expect(pipe).toBeTruthy();
   });
 });
