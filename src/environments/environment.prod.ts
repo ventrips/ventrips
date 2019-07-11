7b189ebf51fbf128eea927eafe8fe7280ee8c8f5
Johnson Huynh <huynhjjk@gmail.com>
2019-07-11 15:22:02 -0700

    updating

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,10 +2,10 @@ export const environment = {
   production: true,
   name: 'Ventrips',
   description: 'A place where you can get all of the latest news and trends',
-  url: 'https://ventrips-website.firebaseapp.com',
+  url: 'https://ventrips.com',
   firebase: {
     apiKey: 'AIzaSyDAb0y87YBB3GH6Qddod_PDiUqjFxpC1gQ',
-    authDomain: 'ventrips-website.firebaseapp.com',
+    authDomain: 'ventrips.com',
     databaseURL: 'https://ventrips-website.firebaseio.com',
     projectId: 'ventrips-website',
     storageBucket: 'ventrips-website.appspot.com',
