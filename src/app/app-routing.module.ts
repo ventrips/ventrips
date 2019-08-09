c94cd9dafb5641a4c227b0f108e8eea242c1cf37
Johnson Huynh <huynhjjk@gmail.com>
2019-08-09 00:04:39 -0700

    changing routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,26 +6,16 @@ import { AdminGuard } from './guards/admin/admin.guard';
 import { EditorGuard } from './guards/editor/editor.guard';
 import { SubscriberGuard } from './guards/subscriber/subscriber.guard';
 
-/* Pages */
-import { HomeComponent } from './pages/home/home.component';
-import { DetailsComponent } from './pages/details/details.component';
-import { AdminComponent } from './pages/admin/admin.component';
-
 const routes: Routes = [
-  {
-    path: '',
-    children: [
-      { path: '', loadChildren: () => import('./pages/home/home.module').then(mod => mod.HomeModule)},
-      { path: 'blog/:slug', loadChildren: () => import('./pages/details/details.module').then(mod => mod.DetailsModule)},
-      { path: 'profile/:uid', loadChildren: () => import('./pages/profile/profile.module').then(mod => mod.ProfileModule)},
-      { path: 'johnson-huynh', loadChildren: () => import('./pages/profile/profile.module').then(mod => mod.ProfileModule)},
-      { path: 'contact', loadChildren: () => import('./pages/contact/contact.module').then(mod => mod.ContactModule)},
-      { path: 'privacy', loadChildren: () => import('./pages/page/page.module').then(mod => mod.PageModule)},
-      { path: 'terms', loadChildren: () => import('./pages/page/page.module').then(mod => mod.PageModule)},
-      { path: 'about', loadChildren: () => import('./pages/page/page.module').then(mod => mod.PageModule)},
-      { path: 'admin', loadChildren: () => import('./pages/admin/admin.module').then(mod => mod.AdminModule)}
-    ]
-  },
+  { path: '', loadChildren: () => import('./pages/home/home.module').then(mod => mod.HomeModule)},
+  { path: 'blog/:slug', loadChildren: () => import('./pages/details/details.module').then(mod => mod.DetailsModule)},
+  { path: 'profile/:uid', loadChildren: () => import('./pages/profile/profile.module').then(mod => mod.ProfileModule)},
+  { path: 'johnson-huynh', loadChildren: () => import('./pages/profile/profile.module').then(mod => mod.ProfileModule)},
+  { path: 'contact', loadChildren: () => import('./pages/contact/contact.module').then(mod => mod.ContactModule)},
+  { path: 'privacy', loadChildren: () => import('./pages/page/page.module').then(mod => mod.PageModule)},
+  { path: 'terms', loadChildren: () => import('./pages/page/page.module').then(mod => mod.PageModule)},
+  { path: 'about', loadChildren: () => import('./pages/page/page.module').then(mod => mod.PageModule)},
+  { path: 'admin', loadChildren: () => import('./pages/admin/admin.module').then(mod => mod.AdminModule)},
   { path: '**', redirectTo: '', pathMatch: 'full' }
 ];
 
