0f556bd1ce91d2520545c78afbeeb32274123715
Johnson Huynh <huynhjjk@gmail.com>
2019-08-10 14:15:23 -0700

    sentiment

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -2,7 +2,7 @@ import * as functions from 'firebase-functions';
 import * as admin from 'firebase-admin';
 import * as _ from 'lodash';
 import * as puppeteer from 'puppeteer';
-import * as Sentiment from 'sentiment';
+var Sentiment = require('sentiment');
 
 admin.initializeApp();
 const db = admin.firestore();
