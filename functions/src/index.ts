41460257df31b696df5235a69601cf30073d86c6
Johnson Huynh <huynhjjk@gmail.com>
2019-08-13 16:57:45 -0700

    adding lodash

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,5 +1,6 @@
 import * as functions from 'firebase-functions';
 import * as admin from 'firebase-admin';
+import * as _ from 'lodash';
 const Utils = require('./utils');
 const Predict = require('./predict');
 const Trends = require('./trends');
