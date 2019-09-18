3d593ed9e636f647585ba02ae67dd64fbf4c1abe
Johnson Huynh <huynhjjk@gmail.com>
2019-09-18 15:26:12 -0700

    all charts

--- functions/src/trends.ts
+++ functions/src/trends.ts
@@ -300,8 +300,10 @@ exports.trends = async function(request: any, response: any, useMock: boolean = 
                     // Volume must be not be too high than 10 Day OR 90 Day Volume Average
                     // ((regularMarketVolume <= (averageDailyVolume10Day * maxThreshold)) || (regularMarketVolume <= (averageDailyVolume3Month * maxThreshold))) &&
 
+                    // **** Regular Market Percent must be higher than 0.02 ***
+                    ((regularMarketChangePercent >= 0.02) &&
                     // Percents must be higher than 0
-                    ((regularMarketChangePercent > 0) && (fiftyDayAverageChangePercent > 0) && (twoHundredDayAverageChangePercent > 0)) &&
+                    (fiftyDayAverageChangePercent > 0) && (twoHundredDayAverageChangePercent > 0)) &&
                     // Change Percentage between Previous Close & Open cannot be greater than 10%
                     (((regularMarketOpen - regularMarketPreviousClose) / Math.abs(regularMarketPreviousClose)) <= maxOpenCloseChangePercent) &&
                     // Not top losers or  new low
