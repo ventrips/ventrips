d4788716b23f86ac688c7d4055ab82a2aa1e1bed
Johnson Huynh <huynhjjk@gmail.com>
2019-11-04 14:24:50 -0800

    working

--- src/app/pages/calculator/calculator.component.ts
+++ src/app/pages/calculator/calculator.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit, PLATFORM_ID, Inject } from '@angular/core';
 import { isPlatformBrowser, isPlatformServer } from '@angular/common';
 import { ToastrService } from 'ngx-toastr';
-import { Calculator } from './../../interfaces/calculator';
+import { SsrService } from '../../services/firestore/ssr/ssr.service';
 import * as _ from 'lodash';
 
 @Component({
@@ -10,18 +10,104 @@ import * as _ from 'lodash';
   styleUrls: ['./calculator.component.scss']
 })
 export class CalculatorComponent implements OnInit {
-  public form: Calculator = new Calculator('', '', '', '');
+  public _ = _;
+  public form = {
+    persons: [
+      {
+        name: `Solinda`,
+        items: [
+          {
+            name: `SOONTOFU MUSHROOM LESS SPICY`,
+            price: 11.95
+          }
+        ]
+      },
+      {
+        name: `Kimhong & Talia`,
+        items: [
+          {
+            name: `Spicy Pork Ramen`,
+            price: 12.95
+          },
+          {
+            name: `Chicken Katsu Curry`,
+            price: 12.95
+          }
+        ]
+      },
+      {
+        name: `Johnson`,
+        items: [
+          {
+            name: `Chicken & Beef`,
+            price: 13.95
+          }
+        ]
+      },
+      {
+        name: `Shullina`,
+        items: [
+          {
+            name: `Chicken Katsu Curry`,
+            price: 12.95
+          }
+        ]
+      },
+      {
+        name: `Kevin`,
+        items: [
+          {
+            name: `Chicken Katsu Curry`,
+            price: 12.95
+          }
+        ]
+      },
+      {
+        name: `Jennifer & Rob`,
+        items: [
+          {
+            name: `Cod Egg Udon`,
+            price: 17.95
+          },
+          {
+            name: `STF COMBO LA GALBI`,
+            price: 20.95
+          }
+        ]
+      }
+    ],
+    tax: 10,
+    gratuity: 18
+  };
 
   constructor(
     public toastrService: ToastrService,
+    private ssrService: SsrService,
     @Inject(PLATFORM_ID) private platformId: any
   ) { }
 
   ngOnInit() {
+    this.ssrService.setSeo({
+      title: `Split Bill Calculator`,
+      description: `Whether you're eating out or travelling with a group, there may come a time when you need to split the bill and pay your part. This calculator helps you!`
+    }, `calculator`, true)
   }
 
-  onSubmit() {
-    this.toastrService.success(`Form has been submitted.`, 'Submission Success!');
+  calculatePersonTotal(person: any) {
+    const filledItemPrices = _.filter(_.get(person, ['items']), (item) => !_.isEmpty(item) && !_.isNil(item));
+    let total = 0;
+    _.forEach(filledItemPrices, (item) => {
+      total += (item.price + (item.price * (_.get(this.form, ['tax']) / 100)) + (item.price * (_.get(this.form, ['gratuity']) / 100)));
+    })
+    return total;
+  }
+
+  calculateGrandTotal(persons: Array<any>): number {
+    let grandTotal = 0;
+    _.forEach(persons, (person) => {
+      grandTotal += this.calculatePersonTotal(person);
+    });
+    return grandTotal;
   }
 
   isPlatformBrowser() {
