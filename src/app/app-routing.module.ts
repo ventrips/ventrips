a097247e82e898f3d309be89fee8c2d28458fc8c
Johnson Huynh <huynhjjk@gmail.com>
2020-04-12 20:36:01 -0700

    enable trends

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -16,7 +16,7 @@ const routes: Routes = [
   { path: 'terms', loadChildren: () => import('./pages/page/page.module').then(mod => mod.PageModule)},
   { path: 'about', loadChildren: () => import('./pages/page/page.module').then(mod => mod.PageModule)},
   { path: 'admin', canActivate: [AdminGuard], loadChildren: () => import('./pages/admin/admin.module').then(mod => mod.AdminModule)},
-  { path: 'trends', canActivate: [SubscriberGuard], loadChildren: () => import('./pages/trends/trends.module').then(mod => mod.TrendsModule)},
+  { path: 'trends', loadChildren: () => import('./pages/trends/trends.module').then(mod => mod.TrendsModule)},
   { path: 'calculator', loadChildren: () => import('./pages/calculator/calculator.module').then(mod => mod.CalculatorModule)},
   { path: '**', redirectTo: '', pathMatch: 'full' }
 ];
