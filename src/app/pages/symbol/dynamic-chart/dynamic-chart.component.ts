665573ffdb002b69d1700e547cbe0676ab7f8d7c
Johnson Huynh <huynhjjk@gmail.com>
2020-04-27 06:44:05 -0700

    styling

--- src/app/pages/symbol/dynamic-chart/dynamic-chart.component.ts
+++ src/app/pages/symbol/dynamic-chart/dynamic-chart.component.ts
@@ -24,32 +24,32 @@ export class DynamicChartComponent implements OnInit {
     {
       backgroundColor: 'rgba(255, 99, 132, 0.2)',
       borderColor: 'rgba(255, 99, 132, 1)',
-      borderWidth: 1
+      borderWidth: 2
     },
     {
       backgroundColor: 'rgba(54, 162, 235, 0.2)',
       borderColor: 'rgba(54, 162, 235, 1)',
-      borderWidth: 1
+      borderWidth: 2
     },
     {
       backgroundColor: 'transparent',
       borderColor: 'rgba(255, 206, 86, 1)',
-      borderWidth: 1
+      borderWidth: 2
     },
     {
       backgroundColor: 'transparent',
       borderColor: 'rgba(75, 192, 192, 1)',
-      borderWidth: 1
+      borderWidth: 2
     },
     {
       backgroundColor: 'transparent',
       borderColor: 'rgba(153, 102, 255, 1)',
-      borderWidth: 1
+      borderWidth: 2
     },
     {
       backgroundColor: 'transparent',
       borderColor: 'rgba(255, 159, 64, 1)',
-      borderWidth: 1
+      borderWidth: 2
     }
   ];
   public lineChartOptions: (ChartOptions & { annotation: any }) = { annotation: {} };
