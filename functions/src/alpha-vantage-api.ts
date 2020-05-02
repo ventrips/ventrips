0de65cc507a44dce6996a5a2bf43db6f61137fee
Johnson Huynh <huynhjjk@gmail.com>
2020-05-02 09:03:59 -0700

    sync fusion test

--- functions/src/alpha-vantage-api.ts
+++ functions/src/alpha-vantage-api.ts
@@ -24,7 +24,57 @@ const setFirebase = (request: any, response: any, data: any, firebasePath: strin
 }
 
 
-const convertChartData = (intraData: any, dayData: any, interval: string) => {
+// const convertChartData = (intraData: any, dayData: any, interval: string) => {
+//     const fiveMinDates = Object.keys(intraData[`Time Series (${interval})`]).map(date => new Date(date).getTime());
+//     const minDate = `${((new Date(Math.min(...fiveMinDates))).toISOString()).substring(0, 10)} 09:30:00`;
+//     const maxDate = `${((new Date(Math.max(...fiveMinDates))).toISOString()).substring(0, 10)} 09:30:00`;
+
+//     _.forEach(dayData['Time Series (Daily)'], (value: any, key: any) => {
+//         _.set(dayData['Time Series (Daily)'], `${key} 09:30:00`, value);
+//         _.unset(dayData['Time Series (Daily)'], key);
+//     });
+//     _.forEach(dayData['Time Series (Daily)'], (value: any, key: any) => {
+//         if (((new Date(key).getTime()) >= (new Date(minDate).getTime())) && ((new Date(key).getTime()) <= (new Date(maxDate).getTime()))) {
+//             _.set(intraData[`Time Series (${interval})`], key, value);
+//         }
+//     });
+//     let dates: Array<any> = [];
+//     _.forEach(intraData[`Time Series (${interval})`], (value: any, key: any) => {
+//         dates.push(_.assign(value, { date: key }));
+//     });
+//     dates = _.sortBy(dates, 'date');
+
+//     const dateGroup = (item: any) => (item['date']).substring(0, 10);
+
+//     const groupedData = _.groupBy(dates, dateGroup);
+
+//     const chartData = {};
+//     _.forEach(groupedData, (value: any, key: any) => {
+//         const chartDatum = {
+//             date: _.map(value, (item: any) => item['date']),
+//             open: _.map(value, (item: any) => _.toNumber(item['1. open'])),
+//             high: _.map(value, (item: any) => _.toNumber(item['2. high'])),
+//             low: _.map(value, (item: any) => _.toNumber(item['3. low'])),
+//             close: _.map(value, (item: any) => _.toNumber(item['4. close'])),
+//             volume: _.map(value, (item: any) => _.toNumber(item['5. volume']))
+//         }
+//         _.set(chartData, key, chartDatum);
+//     });
+
+//     return {
+//         metaData: {
+//             information: intraData['Meta Data']['1. Information'],
+//             symbol: intraData['Meta Data']['2. Symbol'],
+//             lastRefreshed: intraData['Meta Data']['3. Last Refreshed'],
+//             interval: intraData['Meta Data']['4. Interval'],
+//             outputSize: intraData['Meta Data']['5. Output Size'],
+//             timeZone: intraData['Meta Data']['6. Time Zone'],
+//         },
+//         chartData
+//     }
+// };
+
+const convertChartData2 = (intraData: any, dayData: any, interval: string) => {
     const fiveMinDates = Object.keys(intraData[`Time Series (${interval})`]).map(date => new Date(date).getTime());
     const minDate = `${((new Date(Math.min(...fiveMinDates))).toISOString()).substring(0, 10)} 09:30:00`;
     const maxDate = `${((new Date(Math.max(...fiveMinDates))).toISOString()).substring(0, 10)} 09:30:00`;
@@ -38,28 +88,19 @@ const convertChartData = (intraData: any, dayData: any, interval: string) => {
             _.set(intraData[`Time Series (${interval})`], key, value);
         }
     });
-    let dates: Array<any> = [];
+    let data: Array<any> = [];
     _.forEach(intraData[`Time Series (${interval})`], (value: any, key: any) => {
-        dates.push(_.assign(value, { date: key }));
-    });
-    dates = _.sortBy(dates, 'date');
-
-    const dateGroup = (item: any) => (item['date']).substring(0, 10);
-
-    const groupedData = _.groupBy(dates, dateGroup);
-
-    const chartData = {};
-    _.forEach(groupedData, (value: any, key: any) => {
-        const chartDatum = {
-            date: _.map(value, (item: any) => item['date']),
-            open: _.map(value, (item: any) => _.toNumber(item['1. open'])),
-            high: _.map(value, (item: any) => _.toNumber(item['2. high'])),
-            low: _.map(value, (item: any) => _.toNumber(item['3. low'])),
-            close: _.map(value, (item: any) => _.toNumber(item['4. close'])),
-            volume: _.map(value, (item: any) => _.toNumber(item['5. volume']))
+        const chartObj = {
+            x: key,
+            open: _.toNumber(value['1. open']),
+            high: _.toNumber(value['2. high']),
+            low: _.toNumber(value['3. low']),
+            close: _.toNumber(value['4. close']),
+            volume: _.toNumber(value['5. volume'])
         }
-        _.set(chartData, key, chartDatum);
+        data.push(chartObj);
     });
+    const chartData = _.sortBy(data, 'date');
 
     return {
         metaData: {
@@ -94,9 +135,9 @@ export const getAlphaVantageAPI = functions.runWith({ timeoutSeconds: 540, memor
         intraData = await commonRequest({function: 'TIME_SERIES_INTRADAY', symbol, interval, outputsize: 'full'}, BASE_URL, API_KEY);
         dayData = await commonRequest({function: 'TIME_SERIES_DAILY', symbol, outputsize: 'compact'}, BASE_URL, API_KEY);
         const yahooFinanceData: any = await getSingleYahooFinanceAPI(symbol);
-        data = convertChartData(intraData, dayData, interval);
+        data = convertChartData2(intraData, dayData, interval);
         _.set(data, 'yahooFinance', yahooFinanceData);
-        setFirebase(request, response, data, `symbol/${symbol}`, true);
+        setFirebase(request, response, data, `symbol/${symbol}`, false);
     } catch {
         response.status(500).send('Internal Server Error');
     }
--- src/app/pages/stocks/stocks.module.ts
+++ src/app/pages/stocks/stocks.module.ts
@@ -7,6 +7,7 @@ import { ChartsModule } from 'ng2-charts';
 import { StocksComponent } from './stocks.component';
 import { SymbolComponent } from './symbol/symbol.component';
 import { DynamicChartComponent } from './symbol/dynamic-chart/dynamic-chart.component';
+import { StockChartAllModule, ChartAnnotationService, RangeNavigatorAllModule, ChartAllModule } from '@syncfusion/ej2-angular-charts';
 @NgModule({
   declarations: [
     StocksComponent,
@@ -17,7 +18,8 @@ import { DynamicChartComponent } from './symbol/dynamic-chart/dynamic-chart.comp
     CommonModule,
     StocksRoutingModule,
     SharedModule,
-    ChartsModule
+    ChartsModule,
+    StockChartAllModule, RangeNavigatorAllModule, ChartAllModule
   ],
   schemas: [
     CUSTOM_ELEMENTS_SCHEMA
