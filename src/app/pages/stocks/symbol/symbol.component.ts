35fa1864f069da6ef138fba248207d8e5a8dfa3d
Johnson Huynh <huynhjjk@gmail.com>
2020-05-26 05:39:06 -0700

    1k

--- src/app/pages/stocks/symbol/symbol.component.ts
+++ src/app/pages/stocks/symbol/symbol.component.ts
@@ -106,7 +106,7 @@ export class SymbolComponent implements OnInit {
     overall: 0
   };
   public nizom = {
-    buyingPower: 3000,
+    buyingPower: 1000,
     profit: 0
   };
   public yahooFinanceOpenPrice: number;
