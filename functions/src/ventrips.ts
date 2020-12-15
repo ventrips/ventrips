2cba474f85fe8b684265e5221c6b4ebe26e33b80
Johnson Huynh <huynhjjk@gmail.com>
2020-12-15 14:15:37 -0800

    updating ventrips api

--- functions/src/ventrips.ts
+++ functions/src/ventrips.ts
@@ -20,7 +20,6 @@ export const getTrendingTickerSymbols = functions.runWith({ timeoutSeconds: 540,
         let data: any;
 
         /* Step 1: Get all ticker symbols in US Exchange from Finn Hub */
-
         const finnHubOptions: object = _.assign({
             uri: 'https://finnhub.io/api/v1/stock/symbol',
             qs: {
@@ -37,7 +36,7 @@ export const getTrendingTickerSymbols = functions.runWith({ timeoutSeconds: 540,
 
         /* Step 2: Get all yahoo data stock information of every ticker symbol from Yahoo Finance */
         let yahooFinanceResponse: Array<object> = [];
-        const tickerSymbolChunks: any = _.chunk(finnHubAllTickerSymbols, 2000);
+        const tickerSymbolChunks: any = _.chunk(finnHubAllTickerSymbols, 1500);
         for (const tickerSymbolChunk of tickerSymbolChunks) {
             const yahooFinanceOptions: object = _.assign({
                 uri: 'https://query2.finance.yahoo.com/v7/finance/quote',
@@ -55,7 +54,67 @@ export const getTrendingTickerSymbols = functions.runWith({ timeoutSeconds: 540,
         /* Mock Data */
         // data = require('./../mocks/ventrips/yahoo-finance.json');
 
-        response.send(data);
+        /* Step 3: Filter stocks with custom logic */
+        const minPrice: number = 2;
+        const maxPrice: number = 10;
+        const minRegularMarketVolume: number = 10000000;
+        const minThreshold: number = 0.75;
+        const minFiftyTwoWeekHighChangePercent: number = -0.30;
+        const minFiftyTwoWeekLow: number = 1;
+        const minMarketCap: number = 1000000000;
+
+        data = _.filter(data, (item: object) => {
+            const regularMarketPrice: number = _.get(item, ['regularMarketPrice']);
+
+            const regularMarketVolume: number = _.get(item, ['regularMarketVolume']);
+            const averageDailyVolume10Day: number = _.get(item, ['averageDailyVolume10Day']);
+            const averageDailyVolume3Month: number = _.get(item, ['averageDailyVolume3Month']);
+
+            const regularMarketChangePercent: number = _.get(item, ['regularMarketChangePercent']);
+            const fiftyDayAverageChangePercent: number = _.get(item, ['fiftyDayAverageChangePercent']);
+            const twoHundredDayAverageChangePercent: number = _.get(item, ['twoHundredDayAverageChangePercent']);
+
+            const fiftyTwoWeekLowChangePercent: number = _.get(item, ['fiftyTwoWeekLowChangePercent']);
+            const fiftyTwoWeekHighChangePercent: number = _.get(item, ['fiftyTwoWeekHighChangePercent']);
+
+            const fiftyDayAverage: number = _.get(item, ['fiftyDayAverage']);
+            const twoHundredDayAverage: number = _.get(item, ['twoHundredDayAverage']);
+
+            const fiftyTwoWeekLow: number = _.get(item, ['fiftyTwoWeekLow']);
+            const fiftyTwoWeekHigh: number = _.get(item, ['fiftyTwoWeekHigh']);
+
+            const sharesOutstanding: number = _.get(item, ['sharesOutstanding']);
+
+            const marketCap: number = _.get(item, ['marketCap']);
+
+            // Condition #1: Price must be within target price range
+            return (regularMarketPrice >= minPrice && regularMarketPrice <= maxPrice)
+            // Condition #2: All Volumes must be greater than minRegularMarketVolume
+            && ((regularMarketVolume >= minRegularMarketVolume) && ((averageDailyVolume10Day >= minRegularMarketVolume) || (averageDailyVolume3Month >= minRegularMarketVolume)))
+            // Condition #3: Regular Market Volume must be close to 10-Day Volume Average OR 3-Month Volume Average
+            && (((regularMarketVolume * minThreshold) >= averageDailyVolume10Day) || ((regularMarketVolume * minThreshold) >= averageDailyVolume3Month))
+            // Condition #4: 50-Day Average Change Percent, 200-Day Average Change Percent must be greater than 0
+            && ((fiftyDayAverageChangePercent >= 0) && (twoHundredDayAverageChangePercent >= 0))
+            // Condition #5: 50-Day Average must be higher than 200-Day Average
+            && (fiftyDayAverage >= twoHundredDayAverage)
+            // Condition #6: 52-Week High Change Percent must be greater than minFiftyTwoWeekHighChangePercent
+            && (fiftyTwoWeekHighChangePercent >= minFiftyTwoWeekHighChangePercent)
+            // Condition #8: 52-Week Low Price must be greater than minFiftyTwoWeekLow
+            && (fiftyTwoWeekLow >= minFiftyTwoWeekLow)
+            // Condition #9: Market Cap must be greater than minMarketCap
+            // && (marketCap >= minMarketCap)
+
+
+            // // 50 Day Average must be higher than 200 Day Average
+            // ((fiftyDayAverage >= twoHundredDayAverage)) &&
+            // // 50 and 200 Day Average must be higher than 0
+            // ((fiftyDayAverage >= 0) && (twoHundredDayAverage >= 0)) &&
+        });
+
+        response.send({
+            results: _.get(data, ['length'], 0),
+            data
+        });
     } catch {
         response.status(500).send('Internal Server Error');
     }
